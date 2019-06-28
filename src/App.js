106184b678a5190a33c91dee9b89be3d2dbf821b
jobegood49 <jonathan.s.nicolas@gmail.com>
2019-06-27 23:53:20 -0700

    Test toto

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Bonjour toto</p>
         <a
           className="App-link"
           href="https://reactjs.org"
